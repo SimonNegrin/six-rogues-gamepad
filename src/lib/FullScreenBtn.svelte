0ddf537b2bee326fe67d7074dcf854224a2af77b
Simón Negrín <simon.negrin@gmail.com>
2026-04-26 18:52:58 +0100

    Improve full screen btn

--- src/lib/FullScreenBtn.svelte
+++ src/lib/FullScreenBtn.svelte
@@ -2,19 +2,18 @@
   import Button from "./Button.svelte"
   import { globalState } from "./state.svelte"
 
-  let text = $state("Full screen")
+  const enableText = "Full screen"
+  const disableText = "Exit full screen"
 
   async function onclick(): Promise<void> {
     console.log("onclick")
     globalState.isFullScreen = !globalState.isFullScreen
     if (globalState.isFullScreen) {
-      text = "Exit full screen"
       await document.body.requestFullscreen()
     } else {
-      text = "Full screen"
       await document.exitFullscreen()
     }
   }
 </script>
 
-<Button {onclick}>{text}</Button>
+<Button {onclick}>{globalState.isFullScreen ? disableText : enableText}</Button>
